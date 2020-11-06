7e9969e4e681620b06bbafb263e8f650ee9ed577
Waldemar Graban <waldekgraban@gmail.com>
2020-11-06 21:51:59 +0100

    init cave model + resources

---
+++ app/Http/Controllers/CaveController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cave;
+use Illuminate\Http\Request;
+
+class CaveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cave  $cave
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cave $cave)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cave  $cave
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cave $cave)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cave  $cave
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cave $cave)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cave  $cave
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cave $cave)
+    {
+        //
+    }
+}
